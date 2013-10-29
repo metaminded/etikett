eedb211b1248e6dc9cbc70d4f8916521bad7ad79
Florian Thomas <flo@florianthomas.net>
2013-10-29 16:29:12 +0100

    added tag_chooser input field for simple_form

---
+++ app/inputs/tag_chooser_input.rb
@@ -0,0 +1,19 @@
+class TagChooserInput < SimpleForm::Inputs::Base
+  def input
+    data = {
+      class: object.class.name,
+      id: object.id,
+      tag_type: options[:tag_type],
+      input_class: attribute_name,
+      tag_chooser: true
+    }
+    input_html_options[:value] = object.public_send(reflection.name).map do |t|
+      {id: t.id, text: t.name, locked: false}
+    end.to_json
+    input_html_options[:multiple] = true
+    input_html_options[:data] = data
+    input_html_options[:class] = ''
+    options[:wrapper] = :select2
+    @builder.text_field(attribute_name, input_html_options)
+  end
+end
