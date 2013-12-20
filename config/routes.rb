fd4cbcb40ef0c811b8341a24f0f07de773db3222
Florian Thomas <flo@florianthomas.net>
2013-12-20 15:33:25 +0100

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  namespace :etikett do
-    resources :tags, except: [:show]
+  scope path: '/backend' do
+    namespace :etikett do
+      resources :tags, except: [:show]
+    end
   end
 end
