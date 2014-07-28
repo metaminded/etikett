b7e912c9ddb3e5e572f1cee61cd8905cdc37634f
Peter Horn <peter.horn@provideal.net>
2014-07-28 17:20:35 +0200

    added usage_key method

--- app/models/etikett/tag_mapping.rb
+++ app/models/etikett/tag_mapping.rb
@@ -1,4 +1,8 @@
 class Etikett::TagMapping < ActiveRecord::Base
   belongs_to :tag, class_name: 'Etikett::Tag'
   belongs_to :taggable, polymorphic: true
+
+  def usage_key
+    "#{taggable_type.underscore}.#{typ}"
+  end
 end
