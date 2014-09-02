45108e753e9e3d73641ee0604a73f0ff8f42fdb0
Florian Thomas <flo@florianthomas.net>
2014-09-02 12:00:45 +0200

    makes possibility to create new tags dependent on allowed classes of the specific form_field

--- app/inputs/tag_chooser_input.rb
+++ app/inputs/tag_chooser_input.rb
@@ -1,13 +1,15 @@
 class TagChooserInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
+    allowed_classes = object.class.allowed_etikett_classes[reflection.name.to_sym].present? ? object.class.allowed_etikett_classes[reflection.name.to_sym] : reflection.class_name
+
     data = {
-      class: object.class.allowed_etikett_classes[reflection.name.to_sym].present? ? object.class.allowed_etikett_classes[reflection.name.to_sym] : reflection.class_name,
+      class: allowed_classes,
       id: object.id,
       input_class: attribute_name,
       tag_chooser: true,
       multiple: true,
       href: Rails.application.routes.url_helpers.etikett_tags_path,
-      new_tags_allowed: reflection.class_name == 'Etikett::Tag'
+      new_tags_allowed: Array(allowed_classes) == ['Etikett::Tag']
     }
     input_html_options[:value] = object.public_send(reflection.name).map do |t|
       {id: t.id, text: t.name, locked: false, klass: t.class.name.underscore.gsub(/\//, '_')}
